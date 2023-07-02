4ef471e638e031978bb5ad11eefc7ff07ed1a09a
Philipp Daun <post@philippdaun.net>
2023-07-02 15:03:31 +0200

    Refactor for swup 4

--- src/index.js
+++ src/index.js
@@ -3,51 +3,52 @@ import Plugin from '@swup/plugin';
 export default class SwupLivewirePlugin extends Plugin {
 	name = 'SwupLivewirePlugin';
 
+	requires = { swup: '>=4' };
+
+	livewireComponents = [];
+
+	get url() {
+		window.location.pathname + window.location.search;
+	}
+
 	constructor() {
 		super();
 
 		if (typeof window.Livewire === 'undefined') {
 			throw 'window.Livewire is undefined. Make sure to include @livewireScripts before this script';
 		}
-
-		this.livewireComponents = [];
 	}
 
 	mount() {
-		this.swup.on('clickLink', () => this.cacheLivewireComponents());
-		this.swup.on('contentReplaced', () => this.refreshLivewireComponents());
+		this.swup.hooks.before('replaceContent', this.cacheLivewireComponents);
+		this.swup.hooks.on('replaceContent', this.refreshLivewireComponents);
 	}
 
 	unmount() {
-		// this is executed when swup with plugin is disabled
-		// you can use this.swup here to access swup instance
+		this.swup.hooks.off('replaceContent', this.cacheLivewireComponents);
+		this.swup.hooks.off('replaceContent', this.refreshLivewireComponents);
 	}
 
-	cacheLivewireComponents() {
+	cacheLivewireComponents = () => {
 		// Save Livewire components to refresh later
-		this.livewireComponents[window.location.pathname] = document.querySelectorAll(
-			'[wire\\:id]'
-		);
+		const components = Array.from(document.querySelectorAll('[wire\\:id]'));
+		this.livewireComponents[this.url] = components;
 
 		// Pages with Livewire components should not be cached
-		if (this.livewireComponents[window.location.pathname].length) {
-			this.swup.cache.remove(window.location.pathname);
+		if (components.length) {
+			this.swup.cache.delete(this.url);
 		}
 	}
 
-	refreshLivewireComponents() {
+	refreshLivewireComponents = () => {
 		// Refresh any previously saved Livewire Components
-		if (this.livewireComponents[window.location.pathname]) {
-			this.livewireComponents[window.location.pathname].forEach(function(el) {
-				const component = el.__livewire;
-				const dataObject = {
-					fingerprint: component.fingerprint,
-					serverMemo: component.serverMemo,
-					effects: component.effects
-				};
-				el.setAttribute('wire:initial-data', JSON.stringify(dataObject));
-			});
-		}
+		const components = this.livewireComponents[this.url] || [];
+		components.forEach((el) => {
+			const component = el.__livewire;
+			const { effects, fingerprint, serverMemo } = component;
+			const data = { effects, fingerprint, serverMemo };
+			el.setAttribute('wire:initial-data', JSON.stringify(data));
+		});
 
 		// Aleays restart Livewire
 		window.Livewire.restart();
